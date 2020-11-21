9f4ca0f3fc86ebf41ebc6516575a1be550584219
João Pedro Basso Audibert <hotsjoao@gmail.com>
2020-11-21 13:47:15 -0300

    scrapper done

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,34 @@
 import cheerio from 'cheerio';
 import axiosScrapper from './services/axiosScrapper';
 
+interface IPlayerData {
+  rank: number;
+  name: string;
+  nationality: string;
+  goals: number;
+}
+
 const url =
   'https://www.premierleague.com/stats/top/players/goals?se=-1&cl=-1&iso=-1&po=-1?se=-1';
 
-const scrappingPremierLeague
-  axiosScrapper.get(url);
+const scrappingPremierLeague = async (): Promise<void> => {
+  const response = await axiosScrapper.get(url);
+  const html = response.data;
+  const document = cheerio.load(html);
+
+  const statsTable = document('.statsTableContainer > tr');
+  const topScorers: IPlayerData[] = [];
+
+  statsTable.each((index, stat): void => {
+    const playerData: IPlayerData = {
+      rank: Number(document(stat).find('.rank > strong').text()),
+      name: document(stat).find('.playerName > strong').text(),
+      nationality: document(stat).find('.playerCountry').text(),
+      goals: Number(document(stat).find('.mainStat').text()),
+    };
+
+    topScorers.push(playerData);
+  });
+};
+
+scrappingPremierLeague();
